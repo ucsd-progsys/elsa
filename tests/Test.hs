b02e045bed14000f32dade1662165bb06c05205a
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-03-12 23:45:37 +0530

    categorize tests

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,11 +1,96 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
--- import Control.Exception
--- import Test.Tasty
--- import Common
--- import Data.List (isInfixOf)
--- import qualified Language.Elsa.Types as Nano
--- import qualified Language.Nano.Eval  as Nano
+module Main where
+
+import System.Directory
+import System.Exit
+import System.FilePath
+import System.Environment
+import System.IO
+import System.IO.Error
+import Control.Monad (when)
+import Test.Tasty
+import Test.Tasty.HUnit
+import Text.Printf
+import Language.Elsa
 
 main :: IO ()
-main = return ()
+main = defaultMain =<< group "Tests" [unitTests]
+
+unitTests = group "Unit"
+  [ testGroup "ok"      <$> dirTests "tests/ok"       TestOk
+  , testGroup "further" <$> dirTests "tests/further"  TestPartial
+  , testGroup "invalid" <$> dirTests "tests/invalid"  TestInvalid
+  ]
+
+data Outcome
+  = TestOk
+  | TestPartial
+  | TestInvalid
+  | TestMixed
+  deriving (Eq, Ord, Show)
+
+--------------------------------------------------------------------------------
+dirTests :: FilePath -> Outcome -> IO [TestTree]
+--------------------------------------------------------------------------------
+dirTests root code
+  = do files    <- walkDirectory root
+       let tests = [ root </> rel | f <- files, isTest f, let rel = makeRelative root f ]
+       return    $ mkTest code <$> tests
+
+isTest   :: FilePath -> Bool
+isTest f = takeExtension f `elem` [".lc"]
+
+--------------------------------------------------------------------------------
+mkTest :: Outcome -> FilePath -> TestTree
+--------------------------------------------------------------------------------
+mkTest code file
+  = testCase file $ do
+     res <- doTest file
+     when (res /= code) (assertFailure "Wrong Result")
+
+doTest :: FilePath -> IO Outcome
+doTest f =  resultOutcome . elsa <$> parseFile f
+
+resultOutcome :: [Result a] -> Outcome
+resultOutcome rs = case (oks, invs, parts) of
+                     (True, False, False) -> TestOk
+                     (False, True, False) -> TestInvalid
+                     (False, False, True) -> TestPartial
+                     _                    -> TestMixed
+  where
+    oks          = notNull [ r | r@(OK {})      <- rs ]
+    invs         = notNull [ r | r@(Invalid {}) <- rs ]
+    parts        = notNull [ r | r@(Partial {}) <- rs ]
+    notNull      = not . null
+
+----------------------------------------------------------------------------------------
+-- Generic Helpers
+----------------------------------------------------------------------------------------
+
+group n xs = testGroup n <$> sequence xs
+
+----------------------------------------------------------------------------------------
+walkDirectory :: FilePath -> IO [FilePath]
+----------------------------------------------------------------------------------------
+walkDirectory root
+  = do (ds,fs) <- partitionM doesDirectoryExist . candidates =<< (getDirectoryContents root `catchIOError` const (return []))
+       (fs++) <$> concatMapM walkDirectory ds
+  where
+    candidates fs = [root </> f | f <- fs, not (isExtSeparator (head f))]
+
+partitionM :: Monad m => (a -> m Bool) -> [a] -> m ([a],[a])
+partitionM f = go [] []
+  where
+    go ls rs []     = return (ls,rs)
+    go ls rs (x:xs) = do b <- f x
+                         if b then go (x:ls) rs xs
+                              else go ls (x:rs) xs
+
+-- isDirectory :: FilePath -> IO Bool
+-- isDirectory = fmap Posix.isDirectory . Posix.getFileStatus
+
+concatMapM :: Applicative m => (a -> m [b]) -> [a] -> m [b]
+concatMapM _ []     = pure []
+concatMapM f (x:xs) = (++) <$> f x <*> concatMapM f xs
